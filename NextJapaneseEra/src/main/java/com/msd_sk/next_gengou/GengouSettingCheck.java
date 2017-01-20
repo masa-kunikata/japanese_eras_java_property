4aff8347e465ae4414ffb36807a26b7d88564bfb
kunikata <kunikata@msd-sk.net>
2017-01-20 14:26:34 +0900

    プロパティファイルには、UTCでセットする仕様の様子

---
+++ NextJapaneseEra/src/main/java/com/msd_sk/next_gengou/GengouSettingCheck.java
@@ -0,0 +1,51 @@
+/**
+ *
+ */
+package com.msd_sk.next_gengou;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Locale;
+
+/**
+ * @author msd-sk
+ *
+ */
+public class GengouSettingCheck {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	    long millisec = Long.parseLong(args[0], 10);
+
+	    long[] mss = new long[]{
+	    		1546268400000L, //
+	    		1546300800000L, //
+
+	    };
+	    Arrays.stream(mss).forEach(s -> {
+		    System.out.println();
+		    System.out.println("-----------");
+		    System.out.println();
+		    DateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+		    DateFormat jpFormat = new SimpleDateFormat("GGGGyyyy'年'M'月'd'日'", new Locale("ja", "JP", "JP"));
+
+
+		    System.out.println(isoFormat.format(s));
+		    System.out.println(jpFormat.format(s));
+
+
+		    System.out.println("　　　-----1ms前------");
+
+		    long prev = s - 1;
+
+		    System.out.println(isoFormat.format(prev));
+		    System.out.println(jpFormat.format(prev));
+
+
+	    });
+
+	}
+}
